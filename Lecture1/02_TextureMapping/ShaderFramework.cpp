fb69b985ab6424250226fbd24e864c060b141e01
Dorasima-98 <cjk1516@gmail.com>
2021-02-24 21:12:42 +0900

    Texture mapping 1. please... beware of mistouch 2. setTexture() 3. D3DXMatrixRotationXYZ()

--- Lecture1/02_TextureMapping/ShaderFramework.cpp
+++ Lecture1/02_TextureMapping/ShaderFramework.cpp
@@ -26,22 +26,22 @@
 //----------------------------------------------------------------------
 
 // D3D 관련
-LPDIRECT3D9             gpD3D			= NULL;				// D3D
-LPDIRECT3DDEVICE9       gpD3DDevice		= NULL;				// D3D 장치
+LPDIRECT3D9             gpD3D = NULL;				// D3D
+LPDIRECT3DDEVICE9       gpD3DDevice = NULL;				// D3D 장치
 
 // 폰트
-ID3DXFont*              gpFont			= NULL;
+ID3DXFont* gpFont = NULL;
 
-// 모델 // LP의 D3D의 MESH 타입으로 
-LPD3DXMESH				gpSphere		= NULL;
-
-// 쉐이더  // LP의 D3D의 XEFFECT 타입으로
-LPD3DXEFFECT			gpColorShader	= NULL;
-
+// 모델
+LPD3DXMESH				gpSphere = NULL;
+// 쉐이더
+LPD3DXEFFECT			gpTextureMappingShader	= NULL;
 // 텍스처
-
+LPDIRECT3DTEXTURE9		gpEarthDM				= NULL; // 지구 텍스처를 메모리에 저장
 // 프로그램 이름
-const char*				gAppName		= "초간단 쉐이더 데모 프레임워크";
+const char* gAppName = "초간단 쉐이더 데모 프레임워크";
+// 회전값
+float gRotationY = 0.0f;
 
 
 //-----------------------------------------------------------------------
@@ -49,31 +49,31 @@ const char*				gAppName		= "초간단 쉐이더 데모 프레임워크";
 //-----------------------------------------------------------------------
 
 // 진입점
-INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR, INT )
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 {
-    // 1. 윈도우 클래스를 등록한다.
+	// 1. 윈도우 클래스를 등록한다.
 	// 프로그램이 창을 생성하기 위해서는 제일 먼저 윈도우 클래스를 등록해야한다.
 	// 1) 클래스 크기, 2) 스타일 3) 윈도우 프로시져 포인터 4) 클래스 추가 메모리 5) 윈도우 추가메모리
 	// 6) 핸들러 7) 아이콘 리소스 포인터 8) 커서 리소스 포인터 9) 브러쉬 핸들 10) 메뉴 이름 포인터
 	// 11) 클래스 이름 포인터 12) 아이콘 포인터
 	// 뭐... 정 하나하나가 궁금하면 찾아보자
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,
-                      GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
-                      gAppName, NULL };
-    RegisterClassEx( &wc );
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,
+					  GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+					  gAppName, NULL };
+	RegisterClassEx(&wc);
 
-    // 2. 프로그램 창을 생성한다.
+	// 2. 프로그램 창을 생성한다.
 	// 윈도우 클래스의 인스턴스를 생성한다.
 	// 역시 어떤 인자를 넘기는지 궁금하면 찾아보자
 	DWORD style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX;
-    HWND hWnd = CreateWindow( gAppName, gAppName,
-                              style, CW_USEDEFAULT, 0, WIN_WIDTH, WIN_HEIGHT,
-                              GetDesktopWindow(), NULL, wc.hInstance, NULL );
+	HWND hWnd = CreateWindow(gAppName, gAppName,
+		style, CW_USEDEFAULT, 0, WIN_WIDTH, WIN_HEIGHT,
+		GetDesktopWindow(), NULL, wc.hInstance, NULL);
 
 	// 3. Client Rect 크기가 WIN_WIDTH, WIN_HEIGHT와 같도록 크기를 조정한다.
 	POINT ptDiff;
 	RECT rcClient, rcWindow;
-	
+
 	// 실제 렌더링이 가능한 공간이 Client Rect이다.
 	GetClientRect(hWnd, &rcClient);
 	GetWindowRect(hWnd, &rcWindow);
@@ -81,62 +81,62 @@ INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR, INT )
 	ptDiff.x = (rcWindow.right - rcWindow.left) - rcClient.right;
 	ptDiff.y = (rcWindow.bottom - rcWindow.top) - rcClient.bottom;
 	// 조정한다.
-	MoveWindow(hWnd,rcWindow.left, rcWindow.top, WIN_WIDTH + ptDiff.x, WIN_HEIGHT + ptDiff.y, TRUE);
+	MoveWindow(hWnd, rcWindow.left, rcWindow.top, WIN_WIDTH + ptDiff.x, WIN_HEIGHT + ptDiff.y, TRUE);
 
 	// 4. 이제 창을 보여준다.
-    ShowWindow( hWnd, SW_SHOWDEFAULT );
-    UpdateWindow( hWnd );
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
 
 	// 5. D3D를 비롯한 모든 것을 초기화한다.
-	if( !InitEverything(hWnd) )
+	if (!InitEverything(hWnd))
 		PostQuitMessage(1);
 
 	// 6. 메시지 루프
-    MSG msg;
+	MSG msg;
 	// 일종의 메크로 함수 메모리를 0을 초기화
-    ZeroMemory(&msg, sizeof(msg));
+	ZeroMemory(&msg, sizeof(msg));
 	// WM_QUIT 받기 전까지 계속 돌린다.
-    while(msg.message!=WM_QUIT)
-    {
-		if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) )
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
 		{
-            TranslateMessage( &msg );
-            DispatchMessage( &msg );
-        }
-        else // 메시지가 없으면 게임을 업데이트하고 장면을 그린다
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else // 메시지가 없으면 게임을 업데이트하고 장면을 그린다
 		{
 			PlayDemo();
 		}
-    }
+	}
 	// 이것도 제공 메크로함수이고, 윈도우 클래스의 등록을 해제한다.
-    UnregisterClass( gAppName, wc.hInstance );
-    return 0;
+	UnregisterClass(gAppName, wc.hInstance);
+	return 0;
 }
 
 // 메시지 처리기
-LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch( msg )
-    {
+	switch (msg)
+	{
 	case WM_KEYDOWN:
 		ProcessInput(hWnd, wParam);
 		break;
 
-    case WM_DESTROY:
+	case WM_DESTROY:
 		Cleanup();
-        PostQuitMessage(0);
-        return 0;
-    }
+		PostQuitMessage(0);
+		return 0;
+	}
 	// 처리하지 않는 메시지를 default 기본 처리
-    return DefWindowProc( hWnd, msg, wParam, lParam );
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 // 키보드 입력처리
-void ProcessInput( HWND hWnd, WPARAM keyPress)
+void ProcessInput(HWND hWnd, WPARAM keyPress)
 {
-	switch(keyPress)
+	switch (keyPress)
 	{
-	// ESC 키가 눌리면 프로그램을 종료한다.
+		// ESC 키가 눌리면 프로그램을 종료한다.
 	case VK_ESCAPE:
 		// 윈도우 핸들에게 메시지를 보내는 메크로함수
 		PostMessage(hWnd, WM_DESTROY, 0L, 0L);
@@ -150,7 +150,7 @@ void ProcessInput( HWND hWnd, WPARAM keyPress)
 void PlayDemo()
 {
 	Update();
-    RenderFrame();
+	RenderFrame();
 }
 
 // 게임로직 업데이트
@@ -167,71 +167,76 @@ void RenderFrame()
 	D3DCOLOR bgColour = 0xFF0000FF;	// 배경색상 - 파랑
 
 	// 깔끔하게 색만 남기고, 깊이/스텐실을 clear 한다.
-    gpD3DDevice->Clear( 0, NULL, (D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER), bgColour, 1.0f, 0 );
+	gpD3DDevice->Clear(0, NULL, (D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER), bgColour, 1.0f, 0);
 
 	// 씬을 그리기 시작한다.
-    gpD3DDevice->BeginScene();
-    {
+	gpD3DDevice->BeginScene();
+	{
 		RenderScene();				// 3D 물체등을 그린다.
 		RenderInfo();				// 디버그 정보 등을 출력한다.
-    }
+	}
 	// 씬을 다 그렸으면
 	gpD3DDevice->EndScene();
 
 	// 백버퍼에 있는 씬을 화면에 뿌린다.
-    gpD3DDevice->Present( NULL, NULL, NULL, NULL );
+	gpD3DDevice->Present(NULL, NULL, NULL, NULL);
 }
 
 
 // 3D 물체등을 그린다.
 void RenderScene()
 {
-	// 랜더 몽키에서는 변수 시멘틱을 통해 값들을 대입해주었지만
-	// 여기서는 이 값들을 직접 만들어서 셰이더에 전달해주어야한다.
-	
 	// 뷰 행렬을 만든다.
 	D3DXMATRIXA16 matView;
 	D3DXVECTOR3 vEyePt(0.0f, 0.0f, -200.0f);	// 카메라의 위치 벡터
 	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);	// 바라보는 곳의 위치 벡터
 	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);		// 카메라 위를 가리키는 벡터
-	// 그리고 그 벡터들을 인자로 MatrixLookAtLH()에 넣어주면 뷰 행렬을 만들 수 있다.
-	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec); 
+	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
 
 	// 투영 행렬을 만든다. (원근 투시법)
-	// 원근 투시법이냐, 직교투시법이냐에 따라 함수와 매개변수가 달라진다
 	D3DXMATRIXA16 matProjection;
 	// 위에서 정의했던 값들을 넣어준다.
 	D3DXMatrixPerspectiveFovLH(&matProjection, FOV, ASPECT_RATIO, NEAR_PLANE, FAR_PLANE);
+	
+	// 지구를 y축 중심으로 조금씩 회전 시킨다
+	gRotationY += 0.4f * PI / 180.0f;
+	if (gRotationY > 2 * PI)
+	{
+		gRotationY -= 2 * PI;
+	}
 
 	// 월드 행렬을 만든다.
 	// 월드 행렬은 1. 한 물체의 위치와 방위 2.확장/축소 변환을 합친 것이다.
 	// *** 뷰행렬 및 투영행렬과 달리 각 물체마다 월드행렬을 만들어 주어야한다.
 	D3DXMATRIXA16 matWorld;
-	D3DXMatrixIdentity(&matWorld); // 그냥 단위행렬로 만드는 함수를 호출하자. (0,0,0)
+	D3DXMatrixRotationY(&matWorld,gRotationY); // y축으로 돌리는 월드 행렬을 만든다.
 
 	// 이제 이 값을 셰이더에 전달해주자
 	// 1. 셰이더 안에서 사용하는 변수이름과 2. 위에서 정의한 16비트 매트릭스 포인터를 넣어준다.
-	gpColorShader->SetMatrix("gWorldMatrix", &matWorld);
-	gpColorShader->SetMatrix("gViewMatrix", &matView);
-	gpColorShader->SetMatrix("gProjectionMatrix", &matProjection); 
+	gpTextureMappingShader->SetMatrix("gWorldMatrix", &matWorld);
+	gpTextureMappingShader->SetMatrix("gViewMatrix", &matView);
+	gpTextureMappingShader->SetMatrix("gProjectionMatrix", &matProjection);
 
+	// 셰이더에 텍스쳐를 매핑해주는데 _Tex 라는 접미사가 붙는다.
+	// .fx 파일을 열어서 직접 확인 한 다음에 적어보자
+	gpTextureMappingShader->SetTexture("DiffuseMap_Tex", gpEarthDM);
 
 	// 이제 셰이더가 가진 값들을 그릴 물체들에 적용하라고 GPU에게 명령한다.
 	// 모양과 함수들을 잘 보자
 	UINT numPasses = 0;
-	gpColorShader->Begin(&numPasses, NULL); // Begin()과 End() 사이에
+	gpTextureMappingShader->Begin(&numPasses, NULL); // Begin()과 End() 사이에
 	{
 		for (UINT i = 0; i < numPasses; ++i)
 		{
-			gpColorShader->BeginPass(i); // BeginPass()와 EndPass() 가 있고
+			gpTextureMappingShader->BeginPass(i); // BeginPass()와 EndPass() 가 있고
 			{
 				// 구체를 그린다.
 				gpSphere->DrawSubset(0); // 그 사이에 .x를 그리는 DrawSubset()이 있다.
 			}
-			gpColorShader->EndPass();
+			gpTextureMappingShader->EndPass();
 		}
 	}
-	gpColorShader->End();
+	gpTextureMappingShader->End();
 	// 이렇게 하면 GPU가 gpColorShader를 이용해서 gpSphere를 그린다.
 }
 
@@ -239,17 +244,17 @@ void RenderScene()
 void RenderInfo()
 {
 	// 텍스트 색상
-	D3DCOLOR fontColor = D3DCOLOR_ARGB(255,255,255,255);    
+	D3DCOLOR fontColor = D3DCOLOR_ARGB(255, 255, 255, 255);
 
 	// 텍스트를 출력할 위치
 	RECT rct;
-	rct.left=5;
-	rct.right=WIN_WIDTH / 3;
-	rct.top=5;
+	rct.left = 5;
+	rct.right = WIN_WIDTH / 3;
+	rct.top = 5;
 	rct.bottom = WIN_HEIGHT / 3;
-	 
+
 	// 키 입력 정보를 출력
-	gpFont->DrawText(NULL, "데모 프레임워크\n\nESC: 데모종료", -1, &rct, 0, fontColor );
+	gpFont->DrawText(NULL, "데모 프레임워크\n\nESC: 데모종료", -1, &rct, 0, fontColor);
 }
 
 //------------------------------------------------------------
@@ -258,13 +263,13 @@ void RenderInfo()
 bool InitEverything(HWND hWnd)
 {
 	// D3D를 초기화
-	if( !InitD3D(hWnd) )
+	if (!InitD3D(hWnd))
 	{
 		return false;
 	}
-	
+
 	// 모델, 쉐이더, 텍스처등을 로딩
-	if( !LoadAssets() )
+	if (!LoadAssets())
 	{
 		return false;
 	}
@@ -272,9 +277,9 @@ bool InitEverything(HWND hWnd)
 	// 폰트를 로딩
 	// 1) D3D 디바이스 2) 폰트 높이 3) 폰트 너비 4) 볼드체 5) mipmap 레벨 6) 이텔릭체 여부 7) 문자set 8) 실제 출력폰트가 지정된 속성과 어느정도 비슷해야하는지 설정
 	// 9) 실제 출력폰트가 품질이 어느정도 좋아햐 하는지 설정 8) 피치(가로폭)과 패밀리(가족) 설정 9) 폰트 이름 10) 폰트 저장할 out 포인터
-    if(FAILED(D3DXCreateFont( gpD3DDevice, 20, 10, FW_BOLD, 1, FALSE, DEFAULT_CHARSET, 
-                           OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, (DEFAULT_PITCH | FF_DONTCARE), 
-                           "Arial", &gpFont )))
+	if (FAILED(D3DXCreateFont(gpD3DDevice, 20, 10, FW_BOLD, 1, FALSE, DEFAULT_CHARSET,
+		OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, (DEFAULT_PITCH | FF_DONTCARE),
+		"Arial", &gpFont)))
 	{
 		return false;
 	}
@@ -285,57 +290,60 @@ bool InitEverything(HWND hWnd)
 // D3D 객체 및 장치 초기화
 bool InitD3D(HWND hWnd)
 {
-    // D3D 객체
-    gpD3D = Direct3DCreate9( D3D_SDK_VERSION );
-	if ( !gpD3D )
+	// D3D 객체
+	gpD3D = Direct3DCreate9(D3D_SDK_VERSION);
+	if (!gpD3D)
 	{
 		return false;
 	}
 
-    // D3D장치를 생성하는데 필요한 구조체를 채워넣는다.
-    D3DPRESENT_PARAMETERS d3dpp;
-    ZeroMemory( &d3dpp, sizeof(d3dpp) );
+	// D3D장치를 생성하는데 필요한 구조체를 채워넣는다.
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
 
-	d3dpp.BackBufferWidth				= WIN_WIDTH;	// 렌더링 영역, 백버퍼의 너비
-	d3dpp.BackBufferHeight				= WIN_HEIGHT;	// 백버퍼의 높이
-	d3dpp.BackBufferFormat				= D3DFMT_X8R8G8B8;// 백버퍼의 포멧
-    d3dpp.BackBufferCount				= 1;			
-    d3dpp.MultiSampleType				= D3DMULTISAMPLE_NONE;
-    d3dpp.MultiSampleQuality			= 0;
-    d3dpp.SwapEffect					= D3DSWAPEFFECT_DISCARD;// 백버퍼를 스왑할 때의 효과. 성능상 이게 좋다.
-    d3dpp.hDeviceWindow					= hWnd;
-    d3dpp.Windowed						= TRUE;
-    d3dpp.EnableAutoDepthStencil		= TRUE;
-    d3dpp.AutoDepthStencilFormat		= D3DFMT_D24X8; // 깊이/스텐실 버퍼의 포맷
-    d3dpp.Flags							= D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL;
-    d3dpp.FullScreen_RefreshRateInHz	= 0;
-    d3dpp.PresentationInterval			= D3DPRESENT_INTERVAL_ONE; // 모니터 주사율과 백버퍼를 스왑하는 빈도간의 관계. 이것은 모니터가 수직 동기될 때마다 백버퍼를 스왑해준다.
+	d3dpp.BackBufferWidth = WIN_WIDTH;	// 렌더링 영역, 백버퍼의 너비
+	d3dpp.BackBufferHeight = WIN_HEIGHT;	// 백버퍼의 높이
+	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;// 백버퍼의 포멧
+	d3dpp.BackBufferCount = 1;
+	d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
+	d3dpp.MultiSampleQuality = 0;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;// 백버퍼를 스왑할 때의 효과. 성능상 이게 좋다.
+	d3dpp.hDeviceWindow = hWnd;
+	d3dpp.Windowed = TRUE;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D24X8; // 깊이/스텐실 버퍼의 포맷
+	d3dpp.Flags = D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL;
+	d3dpp.FullScreen_RefreshRateInHz = 0;
+	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE; // 모니터 주사율과 백버퍼를 스왑하는 빈도간의 관계. 이것은 모니터가 수직 동기될 때마다 백버퍼를 스왑해준다.
 	// 게임에서는 성능상 모니터의 수직 동기를 기다리지 않고 곧바로 스왑해 주는 경우가 많다.(D3DPRESENT IMMEDIATE) 단, 이럴 때는 전 프레임과 현재 프레임이 서로 찢겨보이는 부작용이 있다.
 
-    // 위 정보(D3DPRESENT_PARAMETERS 구조체)를 가지고 D3D장치를 생성한다.
-    if( FAILED( gpD3D->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
-									D3DCREATE_HARDWARE_VERTEXPROCESSING,
-                                    &d3dpp, &gpD3DDevice ) ) )
-    {
-        return false;
-    }
+	// 위 정보(D3DPRESENT_PARAMETERS 구조체)를 가지고 D3D장치를 생성한다.
+	if (FAILED(gpD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+		D3DCREATE_HARDWARE_VERTEXPROCESSING,
+		&d3dpp, &gpD3DDevice)))
+	{
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
-// 여기서 모델과 셰이더 파일을 로딩한다.
 bool LoadAssets()
 {
 	// 텍스처 로딩
-	
+	gpEarthDM = LoadTexture("Earth.jpg");
+	if (!gpEarthDM)
+	{
+		return false;
+	}
 	// 쉐이더 로딩
-	gpColorShader = LoadShader("ColorShader.fx");
-	if (!gpColorShader)
+	gpTextureMappingShader = LoadShader("TextureMapping.fx");
+	if (!gpTextureMappingShader)
 	{
 		return false;
 	}
 	// 모델 로딩
-	gpSphere = LoadModel("sphere.x");
+	gpSphere = LoadModel("Sphere.x");
 	if (!gpSphere)
 	{
 		return false;
@@ -344,9 +352,9 @@ bool LoadAssets()
 }
 
 // 쉐이더 로딩
-// .fx 포멧으로 저장된 셰이더 파일을 로딩 및 컴파일 한다.
+// .fx 포멧으로 저장된 셰이더 파일
 // 정점셰이더와 픽셀셰이더를 모두 포함하고 있는 텍스트 파일이다.
-LPD3DXEFFECT LoadShader(const char * filename )
+LPD3DXEFFECT LoadShader(const char* filename)
 {
 	LPD3DXEFFECT ret = NULL;
 
@@ -356,7 +364,8 @@ LPD3DXEFFECT LoadShader(const char * filename )
 #if _DEBUG
 	dwShaderFlags |= D3DXSHADER_DEBUG;
 #endif
-	
+
+	// .fx 파일을 로딩 및 컴파일 한다.
 	// 1) D3D 디바이스 2. 셰이더 파일 이름 3. 셰이더를 컴파일 할 때 추가로 사용할 전처리#define
 	// 4) #include 지시문을 처리할 때 사용할 인터페이스 포인터 5) 셰이더를 컴파일할 때 사용할 플래그
 	// 6) 매개변수 공유에 사용할 이펙트 풀 7) 로딩된 이펙트를 저장할 포인터 8) 컴파일 에러 메시지를 가리키는 포인터
@@ -365,17 +374,18 @@ LPD3DXEFFECT LoadShader(const char * filename )
 
 	// 쉐이더 로딩에 실패한 경우 output창에 쉐이더
 	// 컴파일 에러를 출력한다.
-	if(!ret && pError)
+	if (!ret && pError)
 	{
-		int size	= pError->GetBufferSize();
-		void *ack	= pError->GetBufferPointer();
+		int size = pError->GetBufferSize();
+		void* ack = pError->GetBufferPointer();
 
-		if(ack)
+		if (ack)
 		{
 			char* str = new char[size];
+#pragma warning(suppress : 4996)
 			sprintf(str, (const char*)ack, size);
 			OutputDebugString(str);
-			delete [] str;
+			delete[] str;
 		}
 	}
 
@@ -385,13 +395,12 @@ LPD3DXEFFECT LoadShader(const char * filename )
 // 모델 로딩
 // .x 포맷으로 저장된 모델을 로딩
 // DirectX에서 자체적으로 지원하는 메쉬 포맷이다.
-LPD3DXMESH LoadModel(const char * filename)
+LPD3DXMESH LoadModel(const char* filename)
 {
 	LPD3DXMESH ret = NULL;
-	// 모델을 로딩하는 메크로 함수
 	// 1) 파일명 2) 시스템 메모리에 메쉬를 로딩하기로 설정 3) D3D 장치 4) 인접 삼각형 데이터
 	// 5) 메테리얼 정보 6) 이펙트 인스턴스 7) 메테리얼 수 8) 로딩한 메쉬를 저장할 포인터
-	if ( FAILED(D3DXLoadMeshFromX(filename,D3DXMESH_SYSTEMMEM, gpD3DDevice, NULL,NULL,NULL,NULL, &ret)) )
+	if (FAILED(D3DXLoadMeshFromX(filename, D3DXMESH_SYSTEMMEM, gpD3DDevice, NULL, NULL, NULL, NULL, &ret)))
 	{
 		OutputDebugString("모델 로딩 실패: ");
 		OutputDebugString(filename);
@@ -403,11 +412,11 @@ LPD3DXMESH LoadModel(const char * filename)
 
 // 텍스처 로딩
 // 다양한 포맷으로 저장된 이미지들을 텍스처로 로딩
-LPDIRECT3DTEXTURE9 LoadTexture(const char * filename)
+LPDIRECT3DTEXTURE9 LoadTexture(const char* filename)
 {
 	LPDIRECT3DTEXTURE9 ret = NULL;
 	// 1) D3D 장치 2) 파일명 3) 텍스쳐 저장포인터
-	if ( FAILED(D3DXCreateTextureFromFile(gpD3DDevice, filename, &ret)) )
+	if (FAILED(D3DXCreateTextureFromFile(gpD3DDevice, filename, &ret)))
 	{
 		OutputDebugString("텍스처 로딩 실패: ");
 		OutputDebugString(filename);
@@ -425,7 +434,7 @@ LPDIRECT3DTEXTURE9 LoadTexture(const char * filename)
 void Cleanup()
 {
 	// 폰트를 release 한다.
-	if(gpFont)
+	if (gpFont)
 	{
 		gpFont->Release();
 		gpFont = NULL;
@@ -438,23 +447,27 @@ void Cleanup()
 		gpSphere = NULL;
 	}
 	// 쉐이더를 release 한다.
-	if (gpColorShader)
+	if (gpTextureMappingShader)
 	{
-		gpColorShader->Release();
-		gpColorShader = NULL;
+		gpTextureMappingShader->Release();
+		gpTextureMappingShader = NULL;
 	}
 	// 텍스처를 release 한다.
-
-	// 마지막으로 D3D를 release 한다.
-    if(gpD3DDevice)
+	if (gpEarthDM)
 	{
-        gpD3DDevice->Release();
+		gpEarthDM->Release();
+		gpEarthDM = NULL;
+	}
+	// D3D를 release 한다.
+	if (gpD3DDevice)
+	{
+		gpD3DDevice->Release();
 		gpD3DDevice = NULL;
 	}
 
-    if(gpD3D)
+	if (gpD3D)
 	{
-        gpD3D->Release();
+		gpD3D->Release();
 		gpD3D = NULL;
 	}
 }
